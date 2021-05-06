a657014ce7709d072359a6be6d615b3da91cd415
ozgurturkiye <ozgurturkiye@gmail.com>
2021-05-06 22:42:21 +0300

    feat: add ckeditor templates

--- ohacms/urls.py
+++ ohacms/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, PostDetailView
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
 ]
--- ohacms/views.py
+++ ohacms/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from .models import Post
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
