fa527bdb36252de6fdd157ff68dcd0beb8b83b7e
ozgurturkiye <ozgurturkiye@gmail.com>
2021-05-12 20:27:34 +0300

    fix: rename requirement.txt to requirements.txt

--- ohacms/tests.py
+++ ohacms/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        Post.objects.create(name='Welcome', title='WelcomeTitle', description='Welcome index',
+                            content='Welcome content', status='y', post_type='sayfa', order=1)
+
+    def test_post_name_is_equal_entered_name(self):
+        """Is Post name equal entered name"""
+        post1 = Post.objects.get(name='Welcome')
+        self.assertEqual(post1.name, 'Welcome')
